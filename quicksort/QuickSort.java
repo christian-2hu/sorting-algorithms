914ea2436eb6c101cf375e3e899517ddfa0b7545
CHR <60179743+vanilla03@users.noreply.github.com>
2022-12-10 10:07:40 -0300

    Setting things up

--- quicksort/QuickSort.java
+++ quicksort/QuickSort.java
@@ -1,4 +1,34 @@
 package quicksort;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class QuickSort {
-  
+   public static void main(String[] args) {
+      var qs = new QuickSort();
+      int[] array = qs.generateArray();
+      qs.printArr(array);
+   }
+   public void quicksort(int[] arr, int low, int high) {
+
+   }
+   public int[] generateArray() {
+      Random random = new Random();
+      int[] num = new int[7];
+      for(int i = 0; i < num.length; i++) {
+            num[i] = random.nextInt(15);
+      }
+      return num;
+   }
+   public void printArr(int[] arr) {
+      System.out.print("[");
+      for(int i = 0; i < arr.length; i++) {
+         if(i == arr.length-1) {
+            System.out.print(arr[i]);
+            break;
+         }
+         System.out.print(String.format("%s, ", arr[i]));
+      }
+      System.out.println("]");
+   }
 }
